835d380a818ce813183e5366ac98e37007d1215b
Hensy Rodriguez <hensyrodriguez4@gmail.com>
2025-08-30 18:48:33 -0400

    Checks if the word is valid

--- Wordle.cpp
+++ Wordle.cpp
@@ -2,8 +2,8 @@
 #include <iomanip>
 #include <fstream>
 #include <random>
+#include <algorithm>
 #include <string>
-#include <string_view>
 #include <filesystem>
 #include <unordered_map>
 #include <vector>
@@ -15,11 +15,11 @@ const std::string_view MY_ANSI_GREEN     {"\x1b[38;2;0;255;0m"};
 
 std::vector<std::string> englishVector{};
 
-/** Visual example on how it'll work
+/**
  * 1. word = plums
  * 2. result = ""
  * 3. wordGuessed = boats
- * 4. print("Doesn't match") attempts go down by 1 max n attempts
+ * 4. print("Doesn't match") attempts go down by 1 max 5 attempts
  * 5.result = ----s
  * 6. Go back to 3 but make a new guess
  **/
@@ -44,7 +44,7 @@ int main(void)
     std::string word{englishVector[dist(gen)]};
     std::string guessed(word.size(), '-');
 
-    unsigned int numGuessed{5};;
+    unsigned int numGuessed{5};
 
     topLeftCurs();
     clearScreen();
@@ -52,6 +52,7 @@ int main(void)
     std::cout << std::setw(60) << std::setfill('=')
               << "|Welcome to Wordle!|" << std::setw(50) << std::setfill('=') << '\n';
 
+    bool validFlag{false}; // Flag to determine if it's valid word or not.
     while(guessed != word && numGuessed > 0)
     {
         std::cout << "Enter your guess: ";
@@ -62,57 +63,77 @@ int main(void)
             std::cout << "Guess must be " << word.size() << " characters.\n";
             continue;
         }
-        
-        for(size_t i = 0; i < word.size(); i++)
+
+        // Makes the user input be all lowercase
+        std::transform(user_guess.begin(), user_guess.end(), user_guess.begin(), [](unsigned char c){ return std::tolower(c); });
+
+        // Check the word vector to make sure it matches a word in the list
+        for(const auto& eng_word: englishVector)
         {
-            if(user_guess[i] == word[i])
+            if(user_guess == eng_word)
             {
-                guessed[i] = word[i];
+                validFlag = true;
+                break;
+            }
+        }
+
+        if(validFlag)
+        {
+            for(size_t i = 0; i < word.size(); i++)
+            {
+                if(user_guess[i] == word[i])
+                {
+                    guessed[i] = word[i];
+                } else {
+                    guessed[i] = '-';
+                }
+            }
+            // rebuild frequency map each guess
+            std::unordered_map<char, int> freq;
+            for (char c : word) {
+                freq[c]++; // Incr the key(int) per each value (char) found
+            }
+
+            // If a guess matches make that color green
+            std::vector<std::string> colors(word.size(), "");
+            for (size_t i = 0; i < word.size(); i++) {
+                if (user_guess[i] == word[i]) {
+                    colors[i] = MY_ANSI_GREEN; // Apply index to be green
+                    freq[user_guess[i]]--; // If matches reduce key freq
+                }
+            }
+
+            // Handle coloring orange or red if valid or duplicate
+            for (size_t i = 0; i < word.size(); i++) {
+                if (!colors[i].empty()) continue; // already green
+                char c = user_guess[i];
+                if (freq[c] > 0) { // If value key is more than 0, dup found
+                    colors[i] = MY_ANSI_ORANGE;
+                    freq[c]--; // Reduce the key's value
+                } else {
+                    colors[i] = MY_ANSI_RED; // No dup and no val thus red
+                }
+            }
+
+            // Modify to index color, then char and then clear ANSI attrib.
+            for (size_t i = 0; i < word.size(); i++) {
+                std::cout << colors[i] << user_guess[i] << ANSI_CLEAR_ATTRIB;
+            }
+
+            if(guessed == word) {
+                break;
             } else {
-                guessed[i] = '-';
+                --numGuessed;
+                std::cout << "\nAttempts left: " << numGuessed << '\n';
             }
-        }
 
-        // rebuild frequency map each guess
-        std::unordered_map<char, int> freq;
-        for (char c : word) {
-            freq[c]++; // Incr the key(int) per each value (char) found
-        }
-
-        // If a guess char matches make that char index color green
-        std::vector<std::string> colors(word.size(), "");
-        for (size_t i = 0; i < word.size(); i++) {
-            if (user_guess[i] == word[i]) {
-                colors[i] = MY_ANSI_GREEN; // Apply index to be green
-                freq[user_guess[i]]--; // If matches reduce key freq
-            }
-        }
-
-        // Handle coloring orange or red if valid or duplicate
-        for (size_t i = 0; i < word.size(); i++) {
-            if (!colors[i].empty()) continue; // already green
-            char c = user_guess[i];
-            if (freq[c] > 0) { // If value key freq is more than 0, dup found
-                colors[i] = MY_ANSI_ORANGE;
-                freq[c]--; // Reduce the key's value
-            } else {
-                colors[i] = MY_ANSI_RED; // No dup and no val thus red
-            }
-        }
-
-        // Modify to index color, then char and then clear ANSI attrib.
-        for (size_t i = 0; i < word.size(); i++) {
-            std::cout << colors[i] << user_guess[i] << ANSI_CLEAR_ATTRIB;
-        }
-
-        if(guessed == word) {
-            break;
-        } else {
-            --numGuessed;
-            std::cout << "\nAttempts left: " << numGuessed << '\n';
+            // Set the flag back to 
+            validFlag = false;
+        } else { // If !validFlag
+            std::cerr << "Word is not in the word bank" << '\n';
         }
         
-    }
+    } // While loop
 
     if(numGuessed == 0)
     {
@@ -122,7 +143,7 @@ int main(void)
         std::cout << "\nYou won! Word was: " << word << std::endl;
     }
 
-     return 0;
+    return 0;
 }
 
 bool fileToVector (const std::filesystem::path& filename)
